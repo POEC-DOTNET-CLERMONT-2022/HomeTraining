5d50b3787fc203d1b260aa5ddf9feb4dc81dc770
Chakhguereev <lioma.chakhguereev.sio@gmail.com>
2022-01-17 14:29:56 +0100

    grid

--- WPFClient/EventHandlers/ObservableObject.cs
+++ WPFClient/EventHandlers/ObservableObject.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WPFClient
 {
-    /*public class ObservableObject : INottifyPropertyChanged
+    public class ObservableObject : INotifyPropertyChanged
     {
-    }*/
+        //Event
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        protected virtual void OnNotifyPropertyChanged([CallerMemberName] string propertyname = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+    }
 }
