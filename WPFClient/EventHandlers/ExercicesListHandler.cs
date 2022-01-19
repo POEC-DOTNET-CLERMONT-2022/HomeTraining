9cb79f729867df8eaf44076b44c6fc829ed6224b
Chakhguereev <lioma.chakhguereev.sio@gmail.com>
2022-01-19 08:56:16 +0100

    pull

--- WPFClient/EventHandlers/ExercicesListHandler.cs
+++ WPFClient/EventHandlers/ExercicesListHandler.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WPFClient.EventHandlers
+namespace WPFClient
 {
     public class ExercicesListHandler : ObservableObject
     {
--- WPFClient/Home.xaml.cs
+++ WPFClient/Home.xaml.cs
@@ -4,8 +4,8 @@ using Ipme.Hometraining.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace WPFClient
 {
@@ -17,30 +17,48 @@ namespace WPFClient
         private readonly IMapper _mapper = ((App)Application.Current).Mapper;
         readonly ObservableCollection<ProgramModel> _programs;
         readonly ObservableCollection<ExerciceModel> _exercices;
+
+        readonly ExercicesListHandler _exercicesHandler = new ExercicesListHandler();
+
         readonly ObservableCollection<ProgramExerciceModel> _programsExercices;
+
 
         public Home()
         {
             InitializeComponent();
+            foreach (var zone in Enum.GetValues(typeof(MuscleArea)))
+            {
+                TabExercicesZone.Items.Add(new TabItem()
+                {
+                    Header = zone,
+                    Content = new ListBox()
+                    {
+
+                    }
+                });
+            }
+            //TODO corriger cette fonction fictive
+            var exercicesModel = _mapper.Map<IEnumerable<ExerciceModel>>(ExerciceApiRest.GetExercicesByZone("ALL"));
+            _exercicesHandler.Exercices = new ObservableCollection<ExerciceModel>(exercicesModel);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var ex = ExerciceApiRest.GetExercicesAsync("https://localhost:7266/api/Exercices");
             List<ExerciceDto> exDto = ex.Result;
-            foreach(var exx in exDto)
+            foreach (var exx in exDto)
             {
                 ListExercice.Items.Add(exx);
             }
-            
-            
+
+
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            ExerciceModel ex = new ExerciceModel(new Guid(),"Squates sautés","",MuscleArea.Jambes,"", new Guid());
-            ExerciceDto exDto =  _mapper.Map<ExerciceDto>(ex);
-            ExerciceApiRest.PostExerciceAsync("https://localhost:7266/api/Exercices",exDto);
+            ExerciceModel ex = new ExerciceModel(new Guid(), "Squates sautés", "", MuscleArea.Jambes, "", new Guid());
+            ExerciceDto exDto = _mapper.Map<ExerciceDto>(ex);
+            ExerciceApiRest.PostExerciceAsync("https://localhost:7266/api/Exercices", exDto);
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
